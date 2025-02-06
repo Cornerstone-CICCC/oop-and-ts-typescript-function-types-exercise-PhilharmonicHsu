cf785db80266dec529cd6e42550c65d1bac89ca1
Phil <xuyuwei19940909@gmail.com>
2025-02-06 12:19:14 -0800

    done

--- dist/ex05.js
+++ dist/ex05.js
@@ -1,11 +1,10 @@
+"use strict";
 // Create a function called sumAllNumbers.
 // This function should accept any number of numbers using a rest parameter.
 // It should return the sum of all the numbers passed to it.
-
-function sumAllNumbers(...numbers: number[]) {
+function sumAllNumbers(...numbers) {
     return numbers.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue
-    },0);
+        return accumulator + currentValue;
+    }, 0);
 }
-
 console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
--- src/ex03.ts
+++ src/ex03.ts
@@ -3,10 +3,16 @@
 // If two numbers are passed, return their sum.
 // If two strings are passed, return their concatenated (combined) value.
 
-function combine(a, b);
-function combine(a, b);
-function combine(a, b) {
-  
+function combine(a: number, b: number): number;
+function combine(a: string, b: string): string;
+function combine(a: any, b: any): any {
+  if (typeof a === 'number') {
+    return a + b;
+  } else if (typeof a === 'string') {
+    return a + b;
+  }
+
+  return a + b;
 }
 
 console.log(combine(10, 20));      // Expected output: 30
